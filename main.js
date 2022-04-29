63e3f3578d995b6b539eb492467903ea949ccd7e
Gopal Sridhara <gsridhara>
2022-04-29 11:13:23 -0700

    added dificulty and proof-of-work implementation

--- main.js
+++ main.js
@@ -42,6 +42,11 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
 
+    // This is a random number used as one component of the block to get desired
+    // number of leading zeroes for hash If hash value has n number of zeroes,
+    // people can assume that the proof-of-work is done.
+    this.nonce = 0;
+
     this.hash = this.calculateHash();
   }
 
@@ -54,20 +59,22 @@ class Block {
   calculateHash() {
     return (SHA256(
                 this.index + this.previousHash + this.timestamp +
-                JSON.stringify(this.data)))
+                JSON.stringify(
+                    this.data + this.nonce  // new random value.
+                    )))
         .toString();
   }
 
   /*
    * We are going to introduce the concept of proof-of-work here
    *   To make consensus work and distributed way to verify and add new blocks
-   * in to the chain and to mAKE SURE No raaandom person  is just adding aa
+   * in to the chain and to mAKE SURE No random person  is just adding a
    * block a proof-of-work has to be done before any person can add a block to
    * the chain, This process is called mining. It is named so because, for the
    * work done by the miner this blockchain rewards with some thing for having
    * utilizing their resources. They are made to solve a mathematical puzzle
    * which is hard in nature and requires a lot of resources. This ensures that
-   *   which ensures that aadding of block to the chain in time controlled.
+   *   that adding of block to the chain is time controlled.
    *
    *   To make this happen satoshi Nakomoto ( person who started this technology
    * in 2009 for crypto currency BITCOIN) defined a hash value which starts with
@@ -77,15 +84,28 @@ class Block {
    * to block chain.
    *
    *   For our educational purposes let us define this difficulty level as 3.
-   * That means we need to get hash value with 3 leading hcars is zero. Only
+   * That means we need to get hash value with 3 leading  zeros. Only
    * then the block can be added to chain.
    *
    *   For making this to happen a "nonce" component is added to the block. This
    * component is " number once ", a random number Which is added to compute
-   * hash which is tried with various values to get has with n number of leading
-   * zeroes.
+   * hash.  This nonce is changed and hash is computed  to get a hash with n
+   * number of leading zeroes.
    *
    */
+
+  mineBlock(difficulty) {
+    // while changing the nonce value we calculaate hash value until it strts
+    // with "n" number of zeroes n is defined by difficulty level
+
+    while (this.calculateHash().substring(0, difficulty) !==
+           Array(difficulty + 1).join('0')) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log('proof-of-work for mined block Hash = ' + this.hash);
+  }
 }
 
 
@@ -108,8 +128,9 @@ of this chain.
 class BlockChain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 7;
   }
-
+  node
 
   createGenesisBlock() {
     const currentDate = new Date().toUTCString();
@@ -129,7 +150,8 @@ class BlockChain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    // newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -163,31 +185,39 @@ let ebcBlockChain = new BlockChain();
 // we create few initial blocks for this chain.
 // First function block.
 
+console.log('functional block 1 mined: ');
+
 ebcBlockChain.addBlock(new Block(
     1, new Date('04/28/2022 10:00:00').toUTCString(),
     {'Name': 'memberA', 'ebcToken': 5}));
 
 // second function block.
+console.log('functional block 2 mined: ');
 
 ebcBlockChain.addBlock(new Block(
     2, new Date('04/28/2022 11:00:00').toUTCString(),
     {'Name': 'memberB', 'ebcToken': 10}));
+console.log('functional block 3 mined: ');
 
-console.log(
-    'Is my ebcBlockChain Valid' +
-    ' : ' + ebcBlockChain.isBlockChainValid());
+// console.log(
+//   'Is my ebcBlockChain Valid' +
+//   ' : ' + ebcBlockChain.isBlockChainValid());
 
 // Third functional block
+console.log('functional block 3 mined: ');
+
 ebcBlockChain.addBlock(new Block(
     1, new Date('04/28/2022 11:10:00').toUTCString(),
     {'Name': 'memberC', 'ebcToken': 15}));
-
+/*
 console.log(
     'Is my ebcBlockChain Valid' +
     ' : ' + ebcBlockChain.isBlockChainValid());
+*/
 
+// 4th function block.
+console.log('functional block 4 mined: ');
 
-// second function block.
 
 ebcBlockChain.addBlock(new Block(
     2, new Date('04/28/2022 11:50:00').toUTCString(),
@@ -195,4 +225,4 @@ ebcBlockChain.addBlock(new Block(
 
 // TODO : write a method to make changes and show the validity of Chain.
 
-// console.log(JSON.stringify(ebcBlockChain, null, 5));
+console.log(JSON.stringify(ebcBlockChain, null, 5));
